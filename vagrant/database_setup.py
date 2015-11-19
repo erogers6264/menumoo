de8fbd9f2e3471a7fb5260733b6ef4d60da161d4
ethan <erogers6264@gmail.com>
2015-11-18 19:20:02 -0700

    Fix imports

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -1,11 +1,9 @@
 
 import sys
 
-from sqlalchemy import
-Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 
-from sqlalchemy.ext.declarative import
-declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import relationship
 
