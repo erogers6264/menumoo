69b04a9562a3e8d6b6033e28b1aedb0b1cc62a6b
Ethan Rogers <eroger6264@gmail.com>
2015-12-20 12:06:38 -0700

    webserver.sql auf Sqlalchemy vorbereiten

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,14 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Restaurant, Menu, Base
+from database_setup import Restaurant, MenuItem, Base
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
 
 class WebServerHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
+			if self.path.endswith("/restaurants"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>"
+
 			if self.path.endswith("/hello"):
 				self.send_response(200)
 				self.send_header('Content-type', 'text/html')
