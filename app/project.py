1bbe44e8c1ee31a3b942946a3571210a7b2bb538
ethan <erogers6264@gmail.com>
2016-01-14 15:37:04 -0700

    Carry out PEP8 fixes
    Implement Delete functionality

--- app/project.py
+++ app/project.py
@@ -12,34 +12,36 @@ session = DBSession()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-	restaurant = session.query(Restaurant).filter_by(restaurant_id = 
-		restaurant_id).one()
-	items = session.query(MenuItem).filter_by(restaurant_id =
-		restaurant_id)
-	return render_template('menu.html', restaurant=restaurant, items=items)
+    restaurant = session.query(Restaurant).filter_by(
+        restaurant_id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
+
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-	if request.method == 'POST':
-		newItem = MenuItem(name = request.form['name'], restaurant_id =
-			restaurant_id)
-		session.add(newItem)
-		session.commit()
-		return redirect(url_for('restaurantMenu', restaurant_id =
-			restaurant_id))
-	else:
-		return render_template('newmenuitem.html', restaurant_id =
-			restaurant_id)
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'],
+                           restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu',
+                                restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html',
+                               restaurant_id=restaurant_id)
 
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/edit/', methods = ['GET', 'POST'])
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/edit/',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, MenuID):
     editedItem = session.query(MenuItem).filter_by(item_id=MenuID).one()
     if request.method == 'POST':
@@ -47,27 +49,32 @@ def editMenuItem(restaurant_id, MenuID):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+        return redirect(url_for('restaurantMenu',
+                                restaurant_id=restaurant_id))
     else:
         # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
         # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
-        return render_template('editmenuitem.html', restaurant_id=restaurant_id,
-        	MenuID=MenuID, item=editedItem)
+        return render_template('editmenuitem.html',
+                               restaurant_id=restaurant_id,
+                               MenuID=MenuID, item=editedItem)
 
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/delete/',
+           methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, MenuID):
-	itemToDelete = session.query(MenuItem).filter_by(item_id = MenuID).one()
-	if request.method == 'POST':
-		session.delete(itemToDelete)
-		session.commit()
-		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    itemToDelete = session.query(MenuItem).filter_by(item_id=MenuID).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('restaurantMenu',
+                                restaurant_id=restaurant_id))
     else:
-    	return render_template('deletemenuitem.html', arg, arg)
+        return render_template('deletemenuitem.html',
+                               restaurant_id=restaurant_id, item=itemToDelete)
 
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
