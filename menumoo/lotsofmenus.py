311f294da80a2cbbd9e7902414d847f407cfcc19
Ethan Rogers <erogers6264@gmail.com>
2016-02-02 11:27:05 -0700

    Change db name

--- menumoo/lotsofmenus.py
+++ menumoo/lotsofmenus.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
  
 from models import Restaurant, Base, MenuItem
  
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///menumoo.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
