963a21fd9bec8995e3bc1c4c968c5e7326a2565c
Ethan Rogers <erogers6264@gmail.com>
2016-02-20 09:57:15 -0700

    Write in sample email password form

--- menumoo/forms.py
+++ menumoo/forms.py
@@ -1,2 +1,11 @@
 #  forms.py
 #  For learning the use of WTForms
+
+from flask.ext.wtf import Form
+from wtforms.fields import TextField, PasswordField
+from wtforms.validators import Required, Email
+
+
+class EmailPasswordForm(Form):
+	email = TextField('Email', validators=[Required(), Email()])
+	password = PasswordField('Password', validators=[Required()])
