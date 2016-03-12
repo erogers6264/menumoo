d5003ff4f9328aede48c101930e1f886d9cf1fa7
Ethan Rogers <erogers6264@gmail.com>
2016-03-11 17:42:48 -0700

    Add a user class to the data model, and corresponding relationships

--- menumoo/models.py
+++ menumoo/models.py
@@ -1,24 +1,47 @@
 from menumoo import db
 
 
-class Restaurant(db.Model):
-    """A simple class for the restaurants in the database"""
-    
-    __tablename__ = 'restaurant'
+class User(db.Model):
+    """This class contains information about the users of MenuMoo"""
 
-    name = db.Column(db.String(80), nullable = False)
-    description = db.Column(db.String(250))
-    restaurant_id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = 'user'
 
+    user_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    email = db.Column(db.String)
+    picture = db.Column(db.String)
 
     #  This function as a property returns the data in an easily 
     #  serializeable format
     @property
     def serialize(self):
         return {
+            'user_id': self.user_id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
+
+class Restaurant(db.Model):
+    """A simple class for the restaurants in the database"""
+    
+    __tablename__ = 'restaurant'
+
+    restaurant_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    description = db.Column(db.String(250))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user = db.relationship(User)
+
+    @property
+    def serialize(self):
+        return {
             'name': self.name,
             'description': self.description,
             'restaurant_id': self.restaurant_id
+            'user_id': self.user_id
         }
     
 
@@ -27,14 +50,16 @@ class MenuItem(db.Model):
 
     __tablename__ = 'menu_item'
 
-    name = db.Column(db.String(80), nullable = False)
     item_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
     course = db.Column(db.String(250))
     description = db.Column(db.String(250))
     price = db.Column(db.String(8))
     restaurant_id = db.Column(db.Integer, db.ForeignKey(
                               'restaurant.restaurant_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     restaurant = db.relationship(Restaurant)
+    user = db.relationship(User)
 
     @property
     def serialize(self):
@@ -45,4 +70,5 @@ class MenuItem(db.Model):
             'description': self.description,
             'price': self.price,
             'restaurant_id': self.restaurant_id
+            'user_id': self.user_id
         }
