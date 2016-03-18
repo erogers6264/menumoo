fb6efa9db628cb96f7d7e3cceb1d3dbb3ea09f2e
ethan <erogers6264@gmail.com>
2016-03-17 20:05:54 -0700

    Remove debug lines

--- menumoo/views.py
+++ menumoo/views.py
@@ -61,18 +61,13 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    print access_token
     #  Exchange the access token for a long lived server-side token for server
     #  to server API calls.
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
-    print app_id
     app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    print app_secret
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
-    print url
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print result
 
     #  Strip expire tag from access token
     token = result.split('&')[0]
@@ -81,7 +76,6 @@ def fbconnect():
     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
-    print data
 
     login_session['username'] = data['name']
     login_session['email'] = data['email']
@@ -90,7 +84,6 @@ def fbconnect():
     #  Get user picture
     url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=300&width=300' % token
     result = h.request(url, 'GET')[1]
-    print result
     data = json.loads(result)
 
     login_session['picture'] = data['data']['url']
